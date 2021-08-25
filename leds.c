49356b5a25e5f8a48b734c0072dc86c79421bd90
Henry <henrynester@gmail.com>
2021-08-24 21:09:24 -0400

    only one LED

--- leds.c
+++ leds.c
@@ -2,27 +2,26 @@
 #include <xc.h>
 #include "time.h"
 
-void leds_init() {
+void leds_init()
+{
     //connection status LED on RC3
     ANSELC3 = 0; //digital
-    TRISC3 = 0; //out
-    
-    //aux status LED on RC2
-    //PowerPyroRCU: used to show power switch status
-    //
-    ANSELC2 = 0;
-    TRISC2 = 0;
-    
-    //flash both LEDs on startup
-    LATC2 = LATC3 = 1;
+    TRISC3 = 0;  //out
+
+    //flash LED on startup
+    LATC3 = 1;
     __delay_ms(1000);
-    LATC2 = LATC3 = 0; //off at end
+    LATC3 = 0; //off at end
 }
 
-void leds_connected(uint8_t connected) {
-    if(connected) {
+void leds_connected(uint8_t connected)
+{
+    if (connected)
+    {
         LATC3 = (time_millis() % 200) < 100;
-    } else {
+    }
+    else
+    {
         LATC3 = (time_millis() % 2000) < 250;
     }
 }
